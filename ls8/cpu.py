e76e33f4b2151d3e0054ca69d6b774760ab7a194
Chris Filkins <filchyboy@gmail.com>
2020-12-06 18:48:19 -0800

    Edits to both cpu and ls8 examples.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -69,17 +69,7 @@ class CPU:
 
         address = 0
 
-        # For now, we've just hardcoded a program:
 
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
 
         f = open(filename)
         program = [line.split("#")[0] for line in f.read().split("\n") if line.split('#')[0] != '']    
